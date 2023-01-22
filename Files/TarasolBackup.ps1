a48801dd0c928a01e9ed6a112627ca015a673a39
Mhd Alghosh <49556550+MohamadAlghish93@users.noreply.github.com>
2023-01-22 13:15:19 -0800

    Update TarasolBackup for read from csv file

--- Files/TarasolBackup.ps1
+++ Files/TarasolBackup.ps1
@@ -1,15 +1,41 @@
-Write-Host "----------  Tarasol Backup  -------------------" 
+Write-Host "----------  Tarasol Backup  -------------------" -ForegroundColor yellow
 
-$Source_API = "C:\inetpub\wwwroot\TarasolAPI4\*"
-$Source_Site = "C:\inetpub\wwwroot\TarasolSite\*"
-$Destination_Backup = "D:\Backup\Tarasol_"+(get-date -f yyyyMMddhhmm)
+$Current_Date = (get-date -f yyyyMMddhhmm)
+$Destination_Backup = "C:\Backup\Tarasol_$Current_Date"
 
-if (-Not (Test-Path $Destination_Backup )){
-       New-Item -Type Directory "$Destination_Backup\API"
-	   New-Item -Type Directory "$Destination_Backup\Site"
-    }
-	 
-Copy-Item  $Source_API  "$Destination_Backup\API" -Recurse
-Copy-Item  $Source_Site  "$Destination_Backup\Site" -Recurse
+Import-Csv "backup.csv" | foreach{ 
+	
+	
+	$Dest_folder = $_.name
+	$Source_publish = $_.publish
+	$Source_Compress =  (get-item $_.publish ).parent.fullname+"\$Dest_folder.zip"
+	
+    echo $Source_Compress
+
+    # Start-Job -ScriptBlock {
+    Compress-Archive -Path  "$Source_publish\*" -DestinationPath $Source_Compress -Update
+    # } -Name $Dest_folder
+
+    # Wait-Job -Name $Dest_folder
+
+	Write-Host "Start: $Source_publish"
+	
+	
+
+	if (-Not (Test-Path "$Destination_Backup\$Dest_folder" )){
+		   New-Item -Type Directory "$Destination_Backup\$Dest_folder"		   
+	}
+		 
+	Copy-Item  $Source_Compress  "$Destination_Backup\$Dest_folder" -Recurse -Force
+	
+    Remove-Item -Path $Source_Compress -Force
+	Write-Host "End: $Source_publish"
+
+    Start-Sleep -s 10
+
+}
+
 Write-Host "----------  done  -------------------" -ForegroundColor yellow 
+
+
 Start-Sleep -s 300
